fcd2058381ff6e39a22fe523451b4c5ddfc7de68
tatiuska <tfweska@gmail.com>
2023-01-17 13:30:25 -0300

    Concatenação de strings

---
+++ exercicio-10.js
@@ -0,0 +1,7 @@
+// Concatenação de strings
+function concatInitials(firstNameInitial, lastNameInitial) {
+    return firstNameInitial + lastNameInitial;
+}
+
+console.log(concatInitials("J", "D"));
+console.log(concatInitials("S", "B"));

d5b8fc7a31b329cbb61699a1f98d384043eb6df6
tatiuska <tfweska@gmail.com>
2023-01-17 14:32:49 -0300

    Palavras com a primeira letra em maiúsculo

---
+++ exercicio-14.js
@@ -0,0 +1,8 @@
+// Capitalize word
+function capitalize(word) {
+    return word[0].toUpperCase() + word.substring(1).toLowerCase();
+};
+
+console.log(capitalize("sam"));
+console.log(capitalize("ALEX"));
+console.log(capitalize("chARLie"));

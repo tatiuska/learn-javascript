f738b30a732405eb335c74316bb9bfff21c604d2
tatiuska <tfweska@gmail.com>
2023-01-20 18:02:21 -0300

    Convertendo número para string

---
+++ exercicio-15.js
@@ -0,0 +1,8 @@
+// função para converter um número em uma string.
+function convertNumberToString(number) {
+    return number.toString();
+};
+
+console.log(convertNumberToString(42));
+console.log(convertNumberToString(97));
+console.log(convertNumberToString(11));

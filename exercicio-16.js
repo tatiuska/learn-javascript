e74732e55c74f55964bcb553b71cb3b1abd51107
tatiuska <tfweska@gmail.com>
2023-01-22 17:27:53 -0300

    Variáveis

---
+++ exercicio-16.js
@@ -0,0 +1,6 @@
+// Definindo uma variável
+// define a variable "count" with value 0
+// then increment it
+
+let count = 0;
+count = count + 1;
---
+++ exercicio-17.js
@@ -0,0 +1,2 @@
+// define a variable "ageLimit" with value 18 that cannot be reassigned
+const ageLimit = 18;

eb9c3c8506f052556d6d459572a0933a79c05e03
tatiuska <tfweska@gmail.com>
2023-01-27 18:43:08 -0300

    Número de elementos numa array

---
+++ exercicio-21.js
@@ -0,0 +1,7 @@
+// função que retorna o número de ítens contidos numa array
+function getNumberOfElements(elements) {
+    return elements.length;
+};
+
+console.log(getNumberOfElements(['a', 'b', 'c']));
+console.log(getNumberOfElements([]));
